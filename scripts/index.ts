1ce7232b78dd6fb00a7a0ccf5cc32c8ec0083bb7
dubfib <81378985+dubfib@users.noreply.github.com>
2024-11-16 14:08:30 -0800

    feat: auto updating readme

--- scripts/index.ts
+++ scripts/index.ts
@@ -5,16 +5,17 @@ import gradleWrapper from "./src/gradleWrapper.ts";
 import gradleScripts from "./src/gradleScripts.ts";
 import gradleProperties from "./src/gradleProperties.ts";
 
+//this file next in line to be rewritten
 (async () => {
     const FORGE_GRADLE = await forgeGradle();
     const SPI_VERSION = await forgeSPI();
 
     const { JAVA_VERSION, MC_VERSION, MCP_VERSION, MAPPING_CHANNEL, MAPPING_VERSION } = await gradleProperties();
 
-    await gradleWrapper('./gradle/wrapper');
-    await gradleScripts('.');
+    await gradleWrapper('../gradle/wrapper');
+    await gradleScripts('..');
 
-    const build_gradle = (await fs.promises.readFile('./scripts/templates/build.gradle')).toString();
+    const build_gradle = (await fs.promises.readFile('./templates/build.gradle')).toString();
 
     const build_gradle_fixed = build_gradle.toString()
         .replace('FORGE_GRADLE', FORGE_GRADLE.toString())
@@ -25,7 +26,16 @@ import gradleProperties from "./src/gradleProperties.ts";
         .replace('MAPPINGS_VERSION', `'${MAPPING_VERSION}'`)
         .replace('SPI_VERSION', `'${SPI_VERSION}'`)
 
-    await fs.promises.writeFile('./build.gradle', build_gradle_fixed);
+    console.log('finished writing to build.gradle');
 
-    console.log('finished writing to build.gradle')
+    await fs.promises.writeFile('../build.gradle', build_gradle_fixed);
+
+    const read_me = (await fs.promises.readFile('./templates/README.md')).toString();
+
+    const read_me_fixed = read_me.toString()
+        .replace('JAVA_VERSION', `${JAVA_VERSION}`)
+        .replace('MINECRAFT_VERSION', `${MC_VERSION}`)
+    await fs.promises.writeFile('../README.md', read_me_fixed);
+
+    console.log('finished writing to README.md');
 })();

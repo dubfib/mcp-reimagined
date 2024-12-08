3426f8997ffff96c2df0cc4af7bed4e2032c7c43
dubfib <81378985+dubfib@users.noreply.github.com>
2024-12-08 01:17:17 -0800

    refactor: clean up

--- scripts/src/convertVersion.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Convert version to major version (ex: 1.21.1 -> 1.21.x)
- * @param version Version to convert (ex: 1.21.1)
- * @returns Converted version (ex: 1.21.x)
- * @author dubfib
-*/
-export default function convertVersion(version: string) {
-    return version.replace(/^(\d+\.\d+)\.\d+$/, '$1.x').toString();
-}

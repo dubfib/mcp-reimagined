14b8b047b4d8cf286bb57ca8f64cede43386418a
dubfib <81378985+dubfib@users.noreply.github.com>
2024-12-08 01:28:16 -0800

    fix: workflow not creating extra releases

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,3 @@
-//note: if your on windows, change from: / to \\ in the paths
-
 import path from "node:path";
 
 import latestBranch from "./src/latestBranch.ts";
@@ -8,6 +6,7 @@ import forgeGradle from "./src/forgeGradle.ts";
 import gradleWrapper from "./src/gradleWrapper.ts";
 import gradleScripts from "./src/gradleScripts.ts";
 import gradleProperties from "./src/gradleProperties.ts";
+import releaseExists from "./src/releaseExists.ts";
 
 (async () => {
     const root = path.join(__dirname, '..');
@@ -15,35 +14,37 @@ import gradleProperties from "./src/gradleProperties.ts";
     const branch = await latestBranch();
     const { JAVA_VERSION, MC_VERSION, MCP_VERSION, MAPPING_CHANNEL, MAPPING_VERSION } = await gradleProperties(branch);
 
-    const FORGE_GRADLE = await forgeGradle();
-    const SPI_VERSION = await forgeSPI();
+    if (await releaseExists(`${MC_VERSION}-${MCP_VERSION}`)) {
+        const FORGE_GRADLE = await forgeGradle();
+        const SPI_VERSION = await forgeSPI();
 
-    await gradleWrapper(path.join(root, 'gradle/wrapper'), branch);
-    await gradleScripts(root, branch);
-        
-    const build_gradle_path = path.join(root, 'scripts/templates/build.gradle');
-    const build_gradle_template = await Bun.file(build_gradle_path).text();
+        await gradleWrapper(path.join(root, 'gradle\\wrapper'), branch);
+        await gradleScripts(root, branch);
 
-    const build_gradle = build_gradle_template
-        .replace('FORGE_GRADLE', String(FORGE_GRADLE ?? 6))
-        .replace('JAVA_VERSION', String(JAVA_VERSION))
-        .replace('MINECRAFT_VERSION', String(MC_VERSION))
-        .replace('MCP_VERSION', String(MCP_VERSION))
-        .replace('MAPPINGS_CHANNEL', String(MAPPING_CHANNEL))
-        .replace('MAPPINGS_VERSION', String(MAPPING_VERSION))
-        .replace('SPI_VERSION', String(SPI_VERSION))
+        const build_gradle_path = path.join(root, 'scripts\\templates\\build.gradle');
+        const build_gradle_template = await Bun.file(build_gradle_path).text();
 
-    await Bun.write(path.join(root, 'build.gradle'), build_gradle);
+        const build_gradle = build_gradle_template
+            .replace('FORGE_GRADLE', String(FORGE_GRADLE ?? 6))
+            .replace('JAVA_VERSION', String(JAVA_VERSION))
+            .replace('MINECRAFT_VERSION', String(MC_VERSION))
+            .replace('MCP_VERSION', String(MCP_VERSION))
+            .replace('MAPPINGS_CHANNEL', String(MAPPING_CHANNEL))
+            .replace('MAPPINGS_VERSION', String(MAPPING_VERSION))
+            .replace('SPI_VERSION', String(SPI_VERSION))
 
-    const readme_path = path.join(root, 'scripts/templates/README.md');
-    const readme_template = await Bun.file(readme_path).text();
+        await Bun.write(path.join(root, 'build.gradle'), build_gradle);
 
-    const readme = readme_template
-        .replace('JAVA_VERSION', String(JAVA_VERSION))
-        .replace('MINECRAFT_VERSION', String(MC_VERSION))
-        .replace('MCP_VERSION', String(MCP_VERSION));
+        const readme_path = path.join(root, 'scripts\\templates\\README.md');
+        const readme_template = await Bun.file(readme_path).text();
 
-    await Bun.write(path.join(root, 'README.md'), readme);
+        const readme = readme_template
+            .replace('JAVA_VERSION', String(JAVA_VERSION))
+            .replace('MINECRAFT_VERSION', String(MC_VERSION))
+            .replace('MCP_VERSION', String(MCP_VERSION));
 
-    console.log(`${MC_VERSION}-${MCP_VERSION}`);
+        await Bun.write(path.join(root, 'README.md'), readme);
+
+        console.log(`${MC_VERSION}-${MCP_VERSION}`);
+    }
 })();
